1920ed3f0168db7d9b4424bd7b3549333212b88b
Maitri-Shekhda <146981119+Maitri-Shekhda@users.noreply.github.com>
2025-03-11 10:24:31 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, Maitri\n";
+  cout << "new comment added\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
